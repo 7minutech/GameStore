a4ce244121aa12afeb4dc8f710146cd0ced02d20
7minutech <elijohnson1107@gmail.com>
2026-03-18 05:57:31 -0500

    feat: /games route

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,15 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new(1, "Street", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
+    new(2, "Street2", "Fighting", 29.99M, new DateOnly(1993, 7, 15)),
+    new(3, "Street3", "Fighting", 39.99M, new DateOnly(1994, 7, 15))
+];
+
+//Get /games
+app.MapGet("/games", () => games);
 
 app.Run();
